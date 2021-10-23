69a68b02283c802b0542c6188f8233e0dea5561b
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-23 10:54:05 -0400

    prior to getting sync support.js
    fixed ID to drop quotes and prefix capital letters
    
    
    replace -
    
    
    refID refers to mangleIDForSwipl
    
    
    emitBash undo DrawIO HTML
    
    
    emitToScript undoes HTML
    
    
    added run.bash
    
    
    working
    
    
    remove debug ; run VSH0
    
    
    rename to namify

--- 5.pl
+++ 5.pl
@@ -1,25 +1,25 @@
 
+cell(0,"").
+cell(1,"").
 cell(q8f1Wc8Eixn7i3I6va0J__1,"").
 cell(q8f1Wc8Eixn7i3I6va0J__2,"").
 cell(q8f1Wc8Eixn7i3I6va0J__34,"").
 cell(q8f1Wc8Eixn7i3I6va0J__35,"").
 cell(q8f1Wc8Eixn7i3I6va0J__6,"").
 cell(q8f1Wc8Eixn7i3I6va0J__8,"").
-cell(x_0,"").
-cell(x_1,"").
 cell(zEKeuJ7PfgvpXLju9ces__1,"").
 cell(zEKeuJ7PfgvpXLju9ces__2,"").
-diagram(x_FeA5kuE7ltZkfkWCjx6k, "").
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__1).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__2).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__34).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__35).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__6).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__8).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, x_0).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, x_1).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, zEKeuJ7PfgvpXLju9ces__1).
-diagramContains(x_FeA5kuE7ltZkfkWCjx6k, zEKeuJ7PfgvpXLju9ces__2).
+diagram(id_FeA5kuE7ltZkfkWCjx6k, "").
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, 0).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, 1).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__1).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__2).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__34).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__35).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__6).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, q8f1Wc8Eixn7i3I6va0J__8).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, zEKeuJ7PfgvpXLju9ces__1).
+diagramContains(id_FeA5kuE7ltZkfkWCjx6k, zEKeuJ7PfgvpXLju9ces__2).
 edge(q8f1Wc8Eixn7i3I6va0J__34, "").
 ellipse(q8f1Wc8Eixn7i3I6va0J__2, "").
 ellipse(q8f1Wc8Eixn7i3I6va0J__8, "").
@@ -34,7 +34,7 @@ height(q8f1Wc8Eixn7i3I6va0J__6, 70).
 height(q8f1Wc8Eixn7i3I6va0J__8, 40).
 height(zEKeuJ7PfgvpXLju9ces__1, 30).
 height(zEKeuJ7PfgvpXLju9ces__2, 30).
-name(x_FeA5kuE7ltZkfkWCjx6k, "Page-1").
+name(id_FeA5kuE7ltZkfkWCjx6k, "Page-1").
 source(q8f1Wc8Eixn7i3I6va0J__34, q8f1Wc8Eixn7i3I6va0J__2).
 target(q8f1Wc8Eixn7i3I6va0J__34, q8f1Wc8Eixn7i3I6va0J__8).
 value(q8f1Wc8Eixn7i3I6va0J__1, "hello").
@@ -42,8 +42,8 @@ value(q8f1Wc8Eixn7i3I6va0J__2, "out").
 value(q8f1Wc8Eixn7i3I6va0J__35, "VSH0").
 value(q8f1Wc8Eixn7i3I6va0J__6, "world").
 value(q8f1Wc8Eixn7i3I6va0J__8, "in").
-value(zEKeuJ7PfgvpXLju9ces__1, "echo hello").
-value(zEKeuJ7PfgvpXLju9ces__2, "echo world").
+value(zEKeuJ7PfgvpXLju9ces__1, "echo hello&lt;br&gt;echo done &amp;gt;/dev/fd/4").
+value(zEKeuJ7PfgvpXLju9ces__2, "read &amp;lt;/dev/fd/3&lt;br&gt;echo world").
 vertex(q8f1Wc8Eixn7i3I6va0J__1, "").
 vertex(q8f1Wc8Eixn7i3I6va0J__2, "").
 vertex(q8f1Wc8Eixn7i3I6va0J__35, "").
@@ -51,19 +51,19 @@ vertex(q8f1Wc8Eixn7i3I6va0J__6, "").
 vertex(q8f1Wc8Eixn7i3I6va0J__8, "").
 vertex(zEKeuJ7PfgvpXLju9ces__1, "").
 vertex(zEKeuJ7PfgvpXLju9ces__2, "").
-width(q8f1Wc8Eixn7i3I6va0J__1, 120).
+width(q8f1Wc8Eixn7i3I6va0J__1, 150).
 width(q8f1Wc8Eixn7i3I6va0J__2, 40).
 width(q8f1Wc8Eixn7i3I6va0J__35, 550).
-width(q8f1Wc8Eixn7i3I6va0J__6, 120).
+width(q8f1Wc8Eixn7i3I6va0J__6, 140).
 width(q8f1Wc8Eixn7i3I6va0J__8, 40).
-width(zEKeuJ7PfgvpXLju9ces__1, 70).
-width(zEKeuJ7PfgvpXLju9ces__2, 70).
-x(q8f1Wc8Eixn7i3I6va0J__1, 120).
+width(zEKeuJ7PfgvpXLju9ces__1, 115).
+width(zEKeuJ7PfgvpXLju9ces__2, 95).
+x(q8f1Wc8Eixn7i3I6va0J__1, 90).
 x(q8f1Wc8Eixn7i3I6va0J__2, 220).
 x(q8f1Wc8Eixn7i3I6va0J__35, 30).
 x(q8f1Wc8Eixn7i3I6va0J__6, 360).
 x(q8f1Wc8Eixn7i3I6va0J__8, 340).
-x(zEKeuJ7PfgvpXLju9ces__1, 145).
+x(zEKeuJ7PfgvpXLju9ces__1, 100).
 x(zEKeuJ7PfgvpXLju9ces__2, 385).
 y(q8f1Wc8Eixn7i3I6va0J__1, 75).
 y(q8f1Wc8Eixn7i3I6va0J__2, 90).
---
+++ run.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+./transpile.bash
+node emitBash.js
+chmod a+x VSH0 hello world
+echo
+echo '*** running VSH0 ***'
+./VSH0
